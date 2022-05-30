54d63f8922f45ed0fd7ec8dd91b4977409bb324f
DianaVargan <106528784+DianaVargan@users.noreply.github.com>
2022-05-30 14:12:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var express=require('express');
+var app=express();
+var server=require('http').createServer(app);
+var { Server } = require("socket.io");
+var io = new Server(server);
+server.listen(3000);
+
+app.get('/', function(request, respons)
+{
+   respons.sendFile(__dirname + '/index.html');
+});
+
+
+users=[];
+connections=[];
+
+io.sockets.on('connection', function(socket)
+{
+  console.log("Успешное подключение");
+  connections.push(socket);
+
+  socket.on('disconnect', function(data)
+  {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Отключено");
+  });
+
+  socket.on('send mess', function(data){
+    io.sockets.emit('new mess', {mess: data.mess, name:  data.name, className: data.className});
+  });
+});
